3016c21b9b82cc5b2a1b1371d1ef028175d6cb94
badawyy-A <foxbadawi06@gmail.com>
2024-01-15 22:32:29 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import SginUp from "./compents/SginUp"
+import NavBar from "./compents/NavBar"
+
+function App() {
+
+  return (
+    <div className="app">
+        <NavBar/>
+        <SginUp/>
+    </div>
+  )
+}
+
+export default App
